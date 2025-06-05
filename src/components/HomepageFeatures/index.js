8ebeb0adc1474346fbcf16af458193208cdaabc8
dongguacute <gudupao2022@gmail.com>
2025-06-05 18:45:09 +0800

    初始化

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,57 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: '简单易懂',
+    description: (
+      <>
+        与视频配套教程
+      </>
+    ),
+  },
+  {
+    title: '文档支持',
+   
+    description: (
+      <>
+        即本网站.
+      </>
+    ),
+  },
+  {
+    title: 'QQ群',
+   
+    description: (
+      <>
+        待编写
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
